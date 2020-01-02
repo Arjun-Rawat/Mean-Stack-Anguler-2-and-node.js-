f4a1cd69927d80144912c263245d3d2a356e7c73
Arjun-Rawat <46833163+Arjun-Rawat@users.noreply.github.com>
2020-01-02 14:55:47 +0530

    deployment

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports={
-  uri:'mongodb://localhost:27017/Mean-angular',
+  // uri:'mongodb://localhost:27017/Mean-angular',
+  uri: 'mongodb+srv://Arjun07:arjun@cluster0-ctagw.mongodb.net/Mean-angular?retryWrites=true&w=majority',
   secret:crypto,
   db:'Mean-angular'
 }
